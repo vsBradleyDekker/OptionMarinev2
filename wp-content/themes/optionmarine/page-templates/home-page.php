f68cb4c3c9de9d268e9304a26929bd814a425b55
vsBradleyDekker <brad@vealesolutions.com.au>
2017-12-21 15:34:57 +1100

    added home-page.php

---
+++ wp-content/themes/optionmarine/page-templates/home-page.php
@@ -0,0 +1,36 @@
+<?php
+/*
+Template Name: Home page
+*/
+
+
+?>
+<?php get_header(); ?>
+<div id="content">
+    <div id="inner-content" class="wrap cf content-wrap">
+        <div class="banner-wrapper">
+            <div class="banner__background"></div>
+                <div class="banner__content">
+                    <div class="row text-center">
+                        <div class="columns small-12 medium-12">
+                            <h1 class="banner__content-title">Look<span class="underline">ing</span> Good</h1>
+                        </div>
+                    </div>
+                    <div class="row text-center">
+                        <div class="columns small-12 medium-12">
+                            <p class="banner__content-subtitle"> Covering every application we have indoor, outdoor marine to commercial fabrics for your next project</p>
+                        </div>
+                    </div>
+                    <div class="columns small-12 medium-12">
+                        <div class="row text-center">
+                            <button class="banner__content-button"><a href="#">Learn More</a></button>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+
+<?php get_template_part('template-parts/affiliates'); ?>
+<?php get_footer(); ?>
